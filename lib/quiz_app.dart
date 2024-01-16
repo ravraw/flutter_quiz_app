3f7d3e1e7b3098fe14be6fb3f8a1a8fba5ada75e
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-15 20:11:48 -0700

    Added 2 new statefulWidget

---
+++ lib/quiz_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizAppState();
+  }
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
