382531c622f61b475c39ff701b704e34d1e59340
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-15 20:32:52 -0700

    Added switchScreen method

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
